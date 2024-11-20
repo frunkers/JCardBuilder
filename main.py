951be1cbfb25298b776dc9adea45d0e609095ce9
Dmitry <dimaruzaev2@gmai.com>
2024-11-21 00:15:56 +0300

    ref

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from PyQt6 import QtCore
 from PyQt6.QtGui import QPixmap, QCursor, QIcon
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QCheckBox, QInputDialog, \
-    QColorDialog
+    QColorDialog, QGraphicsDropShadowEffect
 
 step = 4
 h = 101 * step
@@ -20,8 +20,9 @@ class Example(QMainWindow):
             'title': 'PILLBOX',
             'album': 'KOMM NIGHT REIN',
             'ser_number': '#TMPL003',
-            'lable': 'TEMPLE DRIVE'
+            'lable': 'TEMPLE DRIVE',
         }
+        self.color_content = '#022c22'
 
     def rerender(self):
         self.setGeometry(0, 0, 1000, 800)
@@ -94,7 +95,6 @@ class Example(QMainWindow):
         self.cassete_title.setStyleSheet(
             '#cassete_title {'
             'font-size: 28px;'
-            'font-weight: 700;'
             'font-family: monospace;'
             '}'
         )
@@ -115,7 +115,6 @@ class Example(QMainWindow):
         self.spine_text2.setStyleSheet(
             '#spine_text2 {'
             'font-size: 8px;'
-            'font-weight: 700;'
             '}'
         )
         self.spine_text2.move(round((w_spine - self.spine_text2.sizeHint().width()) / 2), h - 30)
@@ -135,6 +134,11 @@ class Example(QMainWindow):
             round((h - self.back_text.sizeHint().height()) / 2)
         )
 
+        effect_cassete = QGraphicsDropShadowEffect()
+        effect_cassete.setOffset(0, 0)
+        effect_cassete.setBlurRadius(40)
+        self.cassete.setGraphicsEffect(effect_cassete)
+
         self.form = QLabel(self)
         self.form.move(500, 50)
         self.form.setObjectName('form')
@@ -145,16 +149,34 @@ class Example(QMainWindow):
             'border-radius: 6px;'
             'border: 1px solid #022c22;'
             '}'
+
             '#radio-el {'
             'color: #022c22;'
             '}'
+
             '#names_field {'
             'padding-left: 8px;'
             'background: #ecfdf5;'
             'border: 1px solid #022c22;'
             'color: #022c22;'
             '}'
+
+            '#form_btn {'
+            'background: #d6d3d1;'
+            'color: #022c22;'
+            'border: 1px solid #022c22;'
+            'padding: 2px 4px;'
+            'border-radius: 3px;'
+            '}'
+            '#form_btn:hover {'
+            'background: #a8a29e;'
+            '}'
         )
+
+        effect_form = QGraphicsDropShadowEffect()
+        effect_form.setOffset(0, 0)
+        effect_form.setBlurRadius(40)
+        self.form.setGraphicsEffect(effect_form)
 
         self.radio_italic = QCheckBox('Наклонный', self.form)
         self.radio_italic.move(25, 25)
@@ -168,28 +190,24 @@ class Example(QMainWindow):
 
         self.title = QLineEdit(self.form)
         self.title.setPlaceholderText('Название композитора')
-        self.title.setText(self.state['title'])
         self.title.move(25, 135)
         self.title.resize(200, 30)
         self.title.setObjectName('names_field')
 
         self.album = QLineEdit(self.form)
         self.album.setPlaceholderText('Название альбома/трека')
-        self.album.setText(self.state['album'])
         self.album.move(25, 180)
         self.album.resize(200, 30)
         self.album.setObjectName('names_field')
 
         self.ser_number = QLineEdit(self.form)
         self.ser_number.setPlaceholderText('Номер кассаты из тиража')
-        self.ser_number.setText(self.state['ser_number'])
         self.ser_number.move(25, 225)
         self.ser_number.resize(200, 30)
         self.ser_number.setObjectName('names_field')
 
         self.lable = QLineEdit(self.form)
         self.lable.setPlaceholderText('Название лейбла')
-        self.lable.setText(self.state['lable'])
         self.lable.move(25, 270)
         self.lable.resize(200, 30)
         self.lable.setObjectName('names_field')
@@ -234,22 +252,54 @@ class Example(QMainWindow):
         self.color.clicked.connect(self.form_color)
 
         self.save = QPushButton('Сохранить настройки в файл', self.form)
-        self.save.setObjectName('save')
-        self.save.setStyleSheet(
-            '#save {'
-            'background: #d6d3d1;'
-            'color: #022c22;'
-            'border: 1px solid #022c22;'
-            'padding: 2px 4px;'
-            'border-radius: 3px;'
-            '}'
-            '#save:hover {'
-            'background: #a8a29e;'
-            '}'
-        )
+        self.save.setObjectName('form_btn')
         self.save.move(25, 400)
         self.save.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
         self.save.clicked.connect(self.form_save)
+
+        self.form_reset = QPushButton('Очистить форму', self.form)
+        self.form_reset.setObjectName('form_btn')
+        self.form_reset.setStyleSheet = self.save.style()
+        self.form_reset.move(25, 432)
+        self.form_reset.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.form_reset.clicked.connect(self.form_reset_action)
+
+        self.update_placeholders()
+
+    def update_placeholders(self):
+        self.title.setText(self.state['title'])
+        self.album.setText(self.state['album'])
+        self.ser_number.setText(self.state['ser_number'])
+        self.lable.setText(self.state['lable'])
+
+    def update_content(self):
+        self.cassete_title.setText(self.state['title'])
+        self.spine_text.setText(''.join(list(map(lambda x: x + '\n', self.state['album'])))[:-1])
+        self.spine_text.move(round((w_spine - self.spine_text.sizeHint().width()) / 2), 14)
+        self.spine_text2.setText(self.state['ser_number'])
+        self.spine_text2.move(round((w_spine - self.spine_text2.sizeHint().width()) / 2), h - 30)
+        self.back_text.setText(''.join(list(map(lambda x: x + '\n', self.state['lable'])))[:-1])
+
+    def form_reset_action(self):
+        self.state = {
+            'title': 'PILLBOX',
+            'album': 'KOMM NIGHT REIN',
+            'ser_number': '#TMPL003',
+            'lable': 'TEMPLE DRIVE'
+        }
+        self.update_content()
+        self.update_placeholders()
+        self.radio_italic.setChecked(False)
+        self.radio_caps.setChecked(False)
+        self.radio_bold.setChecked(False)
+        self.setStyleSheet(
+            '#cassete_title, #spine_text, #spine_text2, #back_text {'
+            'color: #022c22;'
+            'font-style: normal;'
+            'text-transform: none;'
+            'font-weight: normal;'
+            '}'
+        )
 
     def form_save(self):
         name, ok_pressed = QInputDialog.getText(self, "Сохранить как", "Введите имя файла: ")
@@ -257,9 +307,9 @@ class Example(QMainWindow):
             pass
 
     def form_color(self):
-        self.color = QColorDialog.getColor()
-        if self.color.isValid():
-            pass
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.color_content = color.name()
 
     def form_submit(self):
         a1 = 'normal'
@@ -271,26 +321,22 @@ class Example(QMainWindow):
             a2 = 'uppercase'
         if self.radio_bold.isChecked():
             a3 = '700'
+        print(self.color_content)
         self.setStyleSheet(
             '#cassete_title, #spine_text, #spine_text2, #back_text {' +
             'font-style: ' + a1 + ';' +
             'text-transform: ' + a2 + ';' +
             'font-weight: ' + a3 + ';' +
+            'color: ' + self.color_content + ';' +
             '}'
         )
-
         self.state = {
             'title': self.title.text(),
             'album': self.album.text(),
             'ser_number': self.ser_number.text(),
             'lable': self.lable.text()
         }
-        self.cassete_title.setText(self.state['title'])
-        self.spine_text.setText(''.join(list(map(lambda x: x + '\n', self.state['album'])))[:-1])
-        self.spine_text.move(round((w_spine - self.spine_text.sizeHint().width()) / 2), 14)
-        self.spine_text2.setText(self.state['ser_number'])
-        self.spine_text2.move(round((w_spine - self.spine_text2.sizeHint().width()) / 2), h - 30)
-        self.back_text.setText(''.join(list(map(lambda x: x + '\n', self.state['lable'])))[:-1])
+        self.update_content()
 
 
 if __name__ == '__main__':
